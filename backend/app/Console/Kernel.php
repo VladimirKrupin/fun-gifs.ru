e15de3f37a13f242fc653f8cea8ead36c6ec4b77
VladimirKrupin <vladimir.krupin133@mail.ru>
2019-01-31 16:22:42 +0400

    make autoposting

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Posting\PostingController;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -32,7 +33,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function()
         {
-            (new UserController())->createUser();
+            (new PostingController())->posting();
         })->everyMinute();
 
 
