4ef0e64164d138e934720af73da05d6384af7f55
VladimirKrupin <vladimir.krupin133@mail.ru>
2019-02-01 10:59:30 +0400

    add vk api

--- backend/app/Http/Controllers/Posting/PostingController.php
+++ backend/app/Http/Controllers/Posting/PostingController.php
@@ -2,7 +2,9 @@
 namespace App\Http\Controllers\Posting;
 use App\Http\Models\Post\Post;
 use App\Http\Controllers\Controller;
+use getjump\Vk\Core as Vk;
 use App;
+//use getjump\Vk\Core;
 use Validator;
 
 class PostingController extends Controller
@@ -18,75 +20,33 @@ class PostingController extends Controller
             var_dump($post);
         }
 
-        session_start();
-        require_once('/var/www/yourculture.ru/config.php');
+//        $vk = new Vk;
+        $vk = Vk::getInstance()->apiVersion('5.5')->setToken('677612e294806d4a5ac4d333b7476956677f353d5a0976584d6d10b3af3cac704645b976df27c7f826d6d&expires_in=0&user_id=244842255');
+        //MESSAGES
+        $data = $vk->request('messages.get', ['count' => 200]);
 
-        $img_src = 'img_path';
+        $userMap = [];
+        $userCache = [];
 
-        $client_id = '5668453'; //id приложения
-        $owner_id = '5668453';//id группы, положительное
-        $token = '5668453';//id группы, положительное
-        $v = "5.59";
+        $user = new \getjump\Vk\Wrapper\User($vk);
 
-//Получаем адрес для загрузки фото на сервер
-        $data = file_get_contents("https://api.vk.com/method/photos.getWallUploadServer?access_token=" . $token . "&group_id=" . $owner_id . "&v=" . $v);
-        $data = json_decode($data);
+        $fetchData = function($id) use($user, &$userMap, &$userCache)
+        {
+            if(!isset($userMap[$id]))
+            {
+                $userMap[$id] = sizeof($userCache);
+                $userCache[] = $user->get($id)->response->get();
+            }
 
-        $link = $data->response->upload_url;
+            return $userCache[$userMap[$id]];
+        };
 
-
-        $post_params = array(
-            'photo' => new CURLFile($img_src)
-        );
-
-//Загружаем фото
-        $ch = curl_init($link);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $response = json_decode($response);
-
-        $server = $response->server;
-        $photo = $response->photo;
-        $hash = $response->hash;
-
-        $link2 = "https://api.vk.com/method/photos.saveWallPhoto?access_token=" . $token . "&server=" . $server . "&hash=" . $hash . "&photo=" . $photo . "&group_id=" . $owner_id . "&v=" . $v;
-        $data3 = file_get_contents($link2);
-
-        $data3 = json_decode($data3, true);
-
-        $photo = $data3[response][0][id];
-        $own = $data3[response][0][owner_id];
-        $gf = "_";
-        $photo3 = "photo$own$gf$photo";
-
-        $url = 'https://api.vk.com/method/wall.post?v=$v';
-        $params = array(
-            'owner_id' => $owner_id1,    // Кому отправляем
-            'message' => $message,   // Что отправляем
-            'from_group' => 1,
-            'attachments' => $photo3 . "," . $pageURL,
-            "lat" => $lat,
-            "long" => $long,
-            //       "publish_date" =>  $date_delay,
-            'access_token' => $token,  // access_token можно вбить хардкодом, если работа будет идти из под одного юзера
-            'v' => '5.59',
-        );
-        $result = file_get_contents($url, false, stream_context_create(array(
-            'http' => array(
-                'method' => 'POST',
-                'header' => 'Content-type: application/x-www-form-urlencoded',
-                'content' => http_build_query($params)
-            )
-        )));
-
-        error_log($result);
-        $result = json_decode($result, true);
-        $id = $result[response][post_id]; //id поста в VK
+//REQUEST WILL ISSUE JUST HERE! SINCE __get overrided
+        $data->each(function($key, $value) use($fetchData) {
+            $user = $fetchData($value->user_id);
+            printf("[%s] %s <br>", $user->getName(), $value->body);
+            return;
+        });
 
     }
 
