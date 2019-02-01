b1bc8d6c92cb29df6e79f8792d278fb649cb3121
VladimirKrupin <vladimir.krupin133@mail.ru>
2019-02-01 14:35:40 +0400

    add vk api

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,3 +1,4 @@
 <?php
 Route::get('/oauthvk/', 'OAuths\OauthsController@OAuthVk');
+Route::get('/',function (){return 'main';});
 //Route::any('{all}', function (){return 'api.fun-gifs';})->where('all', '.*');
