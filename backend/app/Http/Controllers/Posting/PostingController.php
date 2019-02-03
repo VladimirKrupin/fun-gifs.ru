998eeacbc76b0e6ef527fa7f09ca963a21c5cd99
Vladimir Krupin <vladimir.krupin133@mail.ru>
2019-02-03 17:10:51 +0400

    save photo

--- backend/app/Http/Controllers/Posting/PostingController.php
+++ backend/app/Http/Controllers/Posting/PostingController.php
@@ -33,19 +33,25 @@ class PostingController extends Controller
             'v' => 5.92,
         );
 
-        $file_upload_link = file_get_contents('https://api.vk.com/method/photos.getWallUploadServer?' . http_build_query($params));
+        $file_upload_link = file_get_contents("https://api.vk.com/method/photos.getWallUploadServer?access_token=".$access_token."&group_id=".$group_id."&v=".$version);
 
         //Получаем адрес для загрузки фото на сервер
         $data=json_decode($file_upload_link);
 
+        var_dump($data);
+
         $link=$data->response->upload_url;
 
         $post_params = array(
-            'photo' => new CURLFile('/var/www/fun-gifs.ru/backend/storage/app/files-store/fun_gifs_2019-01-31 16:56:44_veGd8klMMws.jpg')
+            'photo' => new CURLFile('/var/www/fun-gifs.ru/backend/storage/app/files-store/padqZPNDgog.jpg')
         );
+
 //Загружаем фото
         $ch = curl_init($link);
         curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type:multipart/form-data"
+        ));
         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
         curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
         curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
@@ -57,27 +63,35 @@ class PostingController extends Controller
         $server=$response->server;
         $photo=$response->photo;
         $hash=$response->hash;
-        var_dump($hash);
+        var_dump($response);
+//        var_dump($hash);
 //        $link2 = "https://api.vk.com/method/photos.saveWallPhoto?access_token=".$access_token."&server=".$server."&hash=".$hash."&photo=".$photo."&group_id=".abs($group_id)."&v=".$version;
-        $link2 = "https://api.vk.com/method/photos.saveWallPhoto?access_token=".$access_token."&server=".$server."&hash=".$hash."&photo=".$photo."&v=".$version;
+//        $link2 = "https://api.vk.com/method/photos.saveWallPhoto?access_token=".$access_token."&server=".$server."&hash=".$hash."&photo=".$photo."&v=".$version;
+//        $data3 = file_get_contents($link2);
+        $link2 = "https://api.vk.com/method/photos.saveWallPhoto?access_token=".$access_token."&server=".$server."&hash=".$hash."&photo=".$photo."&group_id=".$group_id."&v=".$version;
         $data3 = file_get_contents($link2);
 
-        var_dump(json_decode($data3));
+        $data3 = json_decode($data3, true);
+        var_dump($data3);
 
+        $photo= $data3['response'][0]['id'];
+        $own= $data3['response'][0]['owner_id'];
+        $gf = "_";
+        $photo3= "photo$own$gf$photo";
 
+        $request_params = array(
+            'owner_id' => -176519720,
+            'message' => 'test wall',
+            'from_group' => 1,
+            'attachments' => $photo3,
+            'access_token' => $access_token,  // access_token можно вбить хардкодом, если работа будет идти из под одного юзера
+            'v' => 5.92,
+        );
 
-//        $request_params = array(
-//            'owner_id' => -176519720,
-//            'message' => 'test wall',
-//            'from_group' => 1,
-//            'access_token' => $access_token,  // access_token можно вбить хардкодом, если работа будет идти из под одного юзера
-//            'v' => 5.92,
-//        );
-//
-//        $get_params = http_build_query($request_params);
-//        $result = json_decode(file_get_contents('https://api.vk.com/method/wall.post?'. $get_params));
-//
-//        var_dump($result);
+        $get_params = http_build_query($request_params);
+        $result = json_decode(file_get_contents('https://api.vk.com/method/wall.post?'. $get_params));
+
+        var_dump($result);
 
 
     }
