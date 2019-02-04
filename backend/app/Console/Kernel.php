4f7977ad2827cdcb2661a2b1f36f89da5e3faa02
Vladimir Krupin <vladimir.krupin133@mail.ru>
2019-02-04 22:18:57 +0400

    set sheduler

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function()
         {
             (new PostingController())->posting();
-        })->hourlyAt(17)->timezone('Europe/Moscow')->unlessBetween('2:00', '6:00');
+        })->hourlyAt(20)->timezone('Europe/Moscow')->unlessBetween('2:00', '6:00');
 
 //        $schedule->call(function()
 //        {
