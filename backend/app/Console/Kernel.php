c554259ad52436616a51aa8036b06782cf13dd9d
Vladimir Krupin <vladimir.krupin133@mail.ru>
2019-02-09 14:15:59 +0400

    make fb

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -76,11 +76,10 @@ class Kernel extends ConsoleKernel
 //            (new PostingController())->posting();
 //        })->everyMinute();
 
-//        $schedule->call(function()
-//        {
-//            (new PostingController())->posting();
-//            (new PostingController())->postingOk();
-//        })->everyMinute();
+        $schedule->call(function()
+        {
+            (new PostingController())->postingFb();
+        })->everyMinute();
 
 //        $schedule->call(function()
 //        {
