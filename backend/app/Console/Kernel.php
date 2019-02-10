f07a15d73a90d9e9d531089675d5ed94d894d594
Vladimir Krupin <vladimir.krupin133@mail.ru>
2019-02-10 11:55:50 +0400

    make autoposting

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -66,10 +66,11 @@ class Kernel extends ConsoleKernel
             (new PostingController())->wallAllPosting();
         })->twiceDaily(22, 23)->timezone('Europe/Moscow')->unlessBetween('2:00', '6:00');
 
-//        $schedule->call(function()
-//        {
+        $schedule->call(function()
+        {
+            (new PostingController())->wallAllPosting();
 //            (new InstagrammController())->sendInstagramm();
-//        })->everyMinute();
+        })->everyMinute();
 
 //        $schedule->call(function()
 //        {
