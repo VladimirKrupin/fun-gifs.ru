5207977e312dd494d45107a820cf22d1581fae59
Vladimir Krupin <vladimir.krupin133@mail.ru>
2019-02-10 12:11:59 +0400

    make autoposting

--- backend/app/Http/Controllers/Posting/PostingController.php
+++ backend/app/Http/Controllers/Posting/PostingController.php
@@ -265,7 +265,7 @@ class PostingController extends Controller
 //        $post = Post::where('status', 0)
 //            ->with('files')
 //            ->first();
-        $post = Post::where('id', 100)
+        $post = Post::where('id', 101)
             ->with('files')
             ->first();
 
