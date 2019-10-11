5971f3e159030277f7d881803b8ba3a9df1807a5
Vladimir Krupin <vladimir.krupin133@gmail.com>
2019-10-11 17:38:19 +0400

    add remove and post methods

--- backend/app/Http/Controllers/Posting/PostingController.php
+++ backend/app/Http/Controllers/Posting/PostingController.php
@@ -337,7 +337,7 @@ class PostingController extends Controller
 
         if ($post) {
             $post = $post->toArray();
-            $posting_status .= "id: {$post['id']} \r\nuser_id: {$post['user_id']} \r\ncomment: {$post['comment']} \r\nstatus: {$post['status']} \r\nfiles: {$post['files']['path']} \r\n";
+            $posting_status .= "id: {$post['id']} \r\nuser_id: {$post['user_id']} \r\ncomment: {$post['comment']} \r\nstatus: {$post['status']} \r\nfiles: {$post['files'][0]['path']} \r\n";
             $status_vk = $this->wallPosting($post);
             if ($status_vk['status'] === 'error'){
                 Mail::to('vladimir.krupin133@gmail.com')->send(new PostingResultError('Ошибка при постинге ВК',$post,'ВК'));
