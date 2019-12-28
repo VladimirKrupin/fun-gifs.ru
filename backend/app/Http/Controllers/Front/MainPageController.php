c176b775ec75896ba4a833ff6e39eac0f0ef7734
Vladimir Krupin <vladimir.krupin133@gmail.com>
2019-12-28 17:51:53 +0400

    add material front page

--- backend/app/Http/Controllers/Front/MainPageController.php
+++ backend/app/Http/Controllers/Front/MainPageController.php
@@ -1,9 +1,18 @@
 <?php
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Http\Models\Post\Post;
 
 class MainPageController extends Controller
 {
+    public function index()
+    {
+        return view('front.index',['posts'=>Post::where('status', 1)->with('files')->take(10)->orderBy('created_at', 'desc')->get()->toArray()]);
+    }
 
+    public function demo()
+    {
+        return view('front.demo');
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,4 +1,6 @@
 <?php
 Route::get('/oauthvk/', 'OAuths\OauthsController@OAuthVk');
-Route::get('/',function (){return 'main';});
+Route::get('/', 'Front\MainPageController@index');
+Route::get('/demo', 'Front\MainPageController@demo');
+
 //Route::any('{all}', function (){return 'api.fun-gifs';})->where('all', '.*');
