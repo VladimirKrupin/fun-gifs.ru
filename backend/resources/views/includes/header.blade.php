0caae3d8059066b3bde4b43d830cac81aa4d9778
Vladimir Krupin <vladimir.krupin133@gmail.com>
2020-01-01 13:43:31 +0400

    optimize page

--- backend/resources/views/includes/header.blade.php
+++ backend/resources/views/includes/header.blade.php
@@ -48,7 +48,7 @@
         </div>
     </div>
 </nav>
-<div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('/assets/img/bg2.jpg');">
+<div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('/assets/img/bg2.webp');">
     <div class="container">
         <div class="row">
             <div class="col-md-8 ml-auto mr-auto">
