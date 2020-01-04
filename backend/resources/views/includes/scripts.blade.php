cb303e631ca70f553d6b3dfa8434dc7c1dc7a17c
Vladimir Krupin <vladimir.krupin133@gmail.com>
2020-01-04 19:10:19 +0400

    add google analytics

--- backend/resources/views/includes/scripts.blade.php
+++ backend/resources/views/includes/scripts.blade.php
@@ -47,3 +47,15 @@
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/56860381" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
+
+<!-- Google analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155272009-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-155272009-1');
+</script>
+<!-- /Google analytics -->
