7047540cb8294fcc7d4099ad9e0ab6b68ddffd6a
Vladimir Krupin <vladimir.krupin133@gmail.com>
2020-04-06 18:49:04 +0400

    add go to site

--- backend/app/Http/Controllers/Posting/PostingController.php
+++ backend/app/Http/Controllers/Posting/PostingController.php
@@ -388,6 +388,8 @@ class PostingController extends Controller
 
             $status_ok = $this->postingOk($post);
 
+            $posting_status .= $status_ok;
+
             if (!$status_ok){
                 $posting_status .= "Error posting OK\r\n";
             }else{
